67d1d35a155f686b3f4031a5e2b8975813eda201
NAVEENAPRIYA J <jnaveenapriya2006@gmail.com>
2026-03-04 15:44:47 +0530

    Authentication system added

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  password: {
+    type: String,
+    required: true
+  },
+
+  role: {
+    type: String,
+    enum: ["student", "instructor", "admin"],
+    default: "student"
+  },
+
+  goal: {
+    type: String
+  },
+
+  studyTime: {
+    type: Number
+  }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", UserSchema);
--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,10 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-
+const authRoutes = require("./routes/authRoutes");
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Adaptive Learning Platform API Running");
